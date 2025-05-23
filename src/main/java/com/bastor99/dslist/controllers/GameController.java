55b4789e73e9f9e94eede6a3c522450c37f7333d
Vitor <vitorbastos195@gmail.com>
2025-05-23 15:20:27 -0300

    Game repository, service, controller

---
+++ src/main/java/com/bastor99/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.bastor99.dslist.controllers;
+
+import com.bastor99.dslist.dto.GameMinDTO;
+import com.bastor99.dslist.entities.Game;
+import com.bastor99.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll(){
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/com/bastor99/dslist/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package com.bastor99.dslist.repositories;
+
+import com.bastor99.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
